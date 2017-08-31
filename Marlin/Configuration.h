632c472b9610dc1ed701516548842f18a638e2e0
Chris Rigter <chris@rigter.com.au>
2017-08-31 21:52:15 +1000

    current config

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -377,7 +377,6 @@
 #if ENABLED(PIDTEMPBED)
 
   //#define PID_BED_DEBUG // Sends debug data to the serial port.
-M304 P273.73 I44.55 D420.45
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
   #define  DEFAULT_bedKp 273.73
